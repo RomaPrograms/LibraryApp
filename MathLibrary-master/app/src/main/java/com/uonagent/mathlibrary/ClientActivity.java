efddf2462fa5ef74f5c2376b25fd177d3534f547
Roman <romasemizon@gmail.com>
2019-12-18 14:08:44 +0300

    Added comments

---
+++ MathLibrary-master/app/src/main/java/com/uonagent/mathlibrary/ClientActivity.java
@@ -0,0 +1,59 @@
+package com.uonagent.mathlibrary;
+
+import android.app.AlertDialog;
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+
+public class ClientActivity extends AppCompatActivity {
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = item -> {
+        Fragment selectedFragment = null;
+        switch (item.getItemId()) {
+            case R.id.navigation_library:
+                setTitle("Библиотека");
+                android.support.v4.app.FragmentTransaction transaction1 = getSupportFragmentManager().beginTransaction();
+                transaction1.replace(R.id.frame, LibraryClientFragment.newInstance(getIntent().getStringExtra("_id")));
+                transaction1.commit();
+                return true;
+            case R.id.navigation_profile:
+                setTitle("Профиль");
+                android.support.v4.app.FragmentTransaction transaction2 = getSupportFragmentManager().beginTransaction();
+                transaction2.replace(R.id.frame, AccountFragment.newInstance(getIntent().getStringExtra("_id")));
+                transaction2.commit();
+                return true;
+            case R.id.navigation_exit:
+
+                onBackPressed();
+        }
+        return true;
+    };
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Выйти из системы?");
+        builder.setNegativeButton("Нет", (dialogInterface, i) -> {
+            dialogInterface.dismiss();
+        });
+        builder.setPositiveButton("Да", (dialogInterface, i) -> {
+            dialogInterface.dismiss();
+            super.onBackPressed();
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_client);
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        navigation.setSelectedItemId(R.id.navigation_library);
+    }
+
+}
